23c3c314c0dab2d520aae07ad9cc22e431433735
leoni <leoni@192.168.25.83>
2019-04-03 19:36:18 -0300

    Added the Methods for Lists and Starter Status.

--- src/football/Team.java
+++ src/football/Team.java
@@ -24,6 +24,45 @@ public class Team {
 	 */
 	public Team(String name, String city, String country) {
 		
+	}	
+	
+	public ArrayList<Goalkeeper> listGoalkeeper() {
+		
+		ArrayList<Goalkeeper> goalkeeperList = new ArrayList<>();
+		return goalkeeperList;
+	}
+	
+	
+	public ArrayList<Defender> listDefender() {
+		
+		ArrayList<Defender> defenderList = new ArrayList<>();
+		return defenderList;
+	}
+	
+	public ArrayList<Midfielder> listMidfielder() {
+		
+		ArrayList<Midfielder> midfielderList = new ArrayList<>();
+		return midfielderList;
+	}
+	
+	public ArrayList<Attacker> listAttacker() {
+		
+		ArrayList<Attacker> attackerList = new ArrayList<>();
+		return attackerList;
+	}
+	
+	public boolean changeStatus() {
+		
+		boolean starter = true;
+		
+		// reads the current status of a player based on it's jersey number
+		
+		
+		// if statement that will switch the current status
+		// if the player is a starter he will become a reserve
+		// if the player is a reserve he will become a starter
+		
+		return starter;
 	}
 	
 	/**
