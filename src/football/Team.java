e77d10b0844ef68684b6905f70702affe0630a92
leoni <leoni@192.168.25.83>
2019-04-08 17:29:08 -0300

    Moved everything related to a Database to the TeamDatabase class and removed from Team.

--- src/football/Team.java
+++ src/football/Team.java
@@ -35,65 +35,7 @@ public class Team {
 		this.country = country;
 	}	
 	
-	/**
-	 * 
-	 * @return the goal keepers list
-	 */
-	public ArrayList<Goalkeeper> listGoalkeeper() {
-		
-		ArrayList<Goalkeeper> goalkeeperList = new ArrayList<>();
-		new Goalkeeper("Marc-André Ter Stegen", "Germany" , 1, false);
-		new Goalkeeper("Jasper Cillessen", "Netherlands", 13, false);
-		return goalkeeperList;
-	}
 	
-	/** 
-	 * @return the defenders list
-	 */
-	public ArrayList<Defender> listDefender() {
-		
-		ArrayList<Defender> defenderList = new ArrayList<>();
-		new Defender("Samuel Umtity", "France" , 23, false);
-		new Defender("Clément Lenglet", "France" , 15, false);
-		new Defender("Gerard Piqué", "Spain" , 3, false);
-		new Defender("Jeison Murillo", "Colombia" , 17, false);
-		new Defender("Jean Claire Todibo", "France" , 6, false);
-		new Defender("Thomas Vermaelen", "Germany" , 24, false);
-		new Defender("Jordi Alba", "Spain" , 18, false);
-		new Defender("Sergi Roberto", "Spain" , 20, false);
-		new Defender("Nelson Semedo", "Portugal" , 2, false);
-		return defenderList;
-	}
-	
-	/**
-	 * @return the Midfielders list
-	 */
-	public ArrayList<Midfielder> listMidfielder() {
-		
-		ArrayList<Midfielder> midfielderList = new ArrayList<>();
-		new Midfielder("Sergio Busquets", "Spain" , 5, false);
-		new Midfielder("Arthur", "Brazil" , 8, false);
-		new Midfielder("Ivan Rakitic", "Croatia" ,4, false);
-		new Midfielder("Carles Aleñá", "Spain" , 21, false);
-		new Midfielder("Arturo Vidal", "Chile" , 22, false);
-		new Midfielder("Rafinha", "Spain" ,12, false);
-		new Midfielder("Philippe Coutinho", "Brazil" , 7, false);
-		return midfielderList;
-	}
-	
-	/**
-	 * @return the Attackers  list
-	 */
-	public ArrayList<Attacker> listAttacker() {
-		
-		ArrayList<Attacker> attackerList = new ArrayList<>();
-		new Attacker("Ousmane Dembélé", "France" , 11, false);
-		new Attacker("Lionel Messi", "Argentina" , 10, false);
-		new Attacker("Malcolm", "Brazil" , 14, false);
-		new Attacker("Luis Suárez", "Uruguay" , 9, false);
-		new Attacker("Kevin-Prince Boateng", "Ghana" , 19, false);
-		return attackerList;
-	}
 	
 	/**
 	 * This is a method for changing the status of a player 
@@ -144,26 +86,7 @@ public class Team {
 			Player.setStarter(true);
 		}		
 		
-	}
-	
-	/**
-	 * Store the squad in an Array List and save it to a file
-	 * @param squad
-	 */
-	public void storeSquad(ArrayList<Player> squad) {
-		
-	}
-	
-	/**
-	 * Load the player's squad from a file
-	 * @return
-	 */
-	public ArrayList<Player> loadSquad() {		
-		
-		ArrayList<Player> squad = new ArrayList<>();
-		return squad;
-		
-	}
+	}	
 	
 	/**
 	 * @return the Team's name
